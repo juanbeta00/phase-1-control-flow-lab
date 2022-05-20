4c5a53f49aa84c2f97100aeaa1384434a201ef6d
Juan <juanbeta00@hotmail.com>
2022-05-20 12:30:22 -0400

    commit tree 48e0782421e56b525440f828fa8f8a578cdc3eaa

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let charge;
+  if (distance > 2500) {
+    charge = 'No can do.';
+  }else if (distance > 2000) {
+    charge = 'I will gladly take your thirty bucks.';
+  }else if (distance > 400) {
+    charge = 'That will be twenty bucks.';
+  } else {
+    charge = 'This one is on me!';
+  }
+  return charge;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let response = '';
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    break;  
+    case 'not as generous':
+    return  'Thank you.';
+    break; 
+    default:
+    return 'Bye.';
+  }
 }
